f18161e80142550752f61427f96d9e2663b99d70
Billy Fallon <wfallon@pingry.org>
2017-09-21 21:24:34 -0400

    test2.py works, ignore 2leg_oauth.py
    test2.py adapted from tutorial
    (https://gist.githubusercontent.com/st4lk/719729c03cf0314179b4/raw/d3b71
    5506ef73bcdc570570a6963974f34bf899a/oauth10_2leg.py)

---
+++ test2.py
@@ -0,0 +1,62 @@
+import random
+import time
+from urllib import quote, urlencode
+import urllib2
+import binascii
+import hashlib
+import hmac
+
+# Constants given by service provider
+CONSUMER_KEY = '54fae896b065003ecf321fa9cbc90624059c127dc'
+CONSUMER_SECRET = 'ac1e375ea2b80491d218cb4a8014c3c3'
+
+
+# Values that choose client application
+
+API_RESOURCE_URL = 'https://api.schoology.com/v1/courses/'
+
+q = lambda x: quote(x, safe="~")
+get_timestamp = lambda: int(time.time())
+get_nonce = lambda: str(str(random.getrandbits(64)) + str(get_timestamp()))
+
+def get_sign(params, url, http_method, oauth_token_secret=""):
+    """returns HMAC-SHA1 sign"""
+    params.sort()
+    normalized_params = urlencode(params)
+    base_string = "&".join((http_method, q(url), q(normalized_params)))
+    sig = hmac.new("&".join([CONSUMER_SECRET, oauth_token_secret]), base_string, hashlib.sha1)
+    return binascii.b2a_base64(sig.digest())[:-1]
+
+# Start oauth 1.0 2-legged process
+# Or maybe it is called 0-legged process, or 1-legged...
+# oauth 1.0a standard explains only 3-legged process, less-legged process
+# is not an oauth 1.0a in fact.
+
+####################################
+# STEP 1: request to server resource
+####################################
+
+# Note, no auth token is present
+params_confirmed_access_token = [
+    ('oauth_consumer_key', CONSUMER_KEY),
+    ('oauth_nonce', get_nonce()),
+    ('oauth_signature_method', "HMAC-SHA1"),
+    ('oauth_timestamp', get_timestamp()),
+    ('oauth_version', '1.0'),
+]
+
+# signature
+signature = get_sign(params_confirmed_access_token, API_RESOURCE_URL, "GET")
+params_confirmed_access_token.append(('oauth_signature', signature))
+
+# GET https://api.bitbucket.org/1.0/repositories/lexev/tutorial/?
+#    oauth_consumer_key=your_app_key&
+#    oauth_nonce=165399630806490086601427390977&
+#    oauth_signature_method=HMAC-SHA1&
+#    oauth_timestamp=1427390977&
+#    oauth_version=1.0&
+#    oauth_signature=aK%2BkRS5B%2BoFzXjMcQ%2FBtTMMELYk%3D
+url = "?".join((API_RESOURCE_URL, urlencode(params_confirmed_access_token)))
+resp = urllib2.urlopen(url)
+assert resp.code == 200
+print resp.read()
